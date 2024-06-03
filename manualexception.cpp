6b22578c05ef32b3c5532d87d11f00db90ee8436
zackynz <nurasfanz@gmail.com>
2024-06-03 15:02:02 +0700

    mendeklarasikan trs untuk menampilkan pengulangan

---
+++ manualexception.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        cout << "Pernyataan tidak ada dieksekusi" << endl;
+    }
+}
