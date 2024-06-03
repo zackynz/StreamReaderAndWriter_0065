dbe038cae8196e0d4973cbb5faee335b0270d530
zackynz <nurasfanz@gmail.com>
2024-06-03 14:56:17 +0700

    mendeklarasikan while untuk melakukan oerulangan setiap baris

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -26,4 +26,14 @@ int main() {
     ifstream infile;
 
     infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            
+        }
+    }
 }
